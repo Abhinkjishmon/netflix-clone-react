2181b2abc794b618ad7dd4dcd6185b0bcc2873b8
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-03-01 21:34:21 +0530

    nav section pending

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Banner from './components/Banner';
+import Nav from './components/Nav';
 import Row from './components/Row';
 import requests from './request';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Banner />
       <Row isPoster={true} title='Netflix Originals' fetchUrl={requests.fetchNetflixOriginals}/>
       <Row title='Trending' fetchUrl={requests.fetchTrending}/>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    <div className='nav'>
+        <img  className='nav_logo'
+        src="https://andrewjamesspooner.com/wp-content/uploads/2019/06/netflix-logo-e1536243210766-1024x284.png" alt="netflix logo" />
+    </div>
+  )
+}
+
+export default Nav
