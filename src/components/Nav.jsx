a258a3443cf6fa7a38a6d15ba2cff8bbf420413c
Abhinkjishmon <abhinkjishmon001@gmail.com>
2023-03-02 21:29:59 +0530

    nav section finished

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Nav.css'
 
 const Nav = () => {
+  const [show,setShow] = useState(false)
+
+  useEffect(()=>{
+    window.addEventListener("scroll",()=>{
+      if(window.screenY>500){
+        setShow(true)
+      }
+      else{
+        setShow(false)
+      }
+    })
+  },[])
+  console.log('show',show);
   return (
-    <div className='nav'>
+    <div className={`nav ${show && "nav_black"}`}>
         <img  className='nav_logo'
         src="https://andrewjamesspooner.com/wp-content/uploads/2019/06/netflix-logo-e1536243210766-1024x284.png" alt="netflix logo" />
     </div>
